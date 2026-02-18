68c6039c194728c9441e188f2a1ec44b5685ea15
Mona Lisa <nimgunathilaka021@gmail.com>
2026-02-18 20:50:05 +0530

    Add file reading demo and initial file setup

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 console.log("Hello from Node.js!");
 console.log("Lab 03 - Introduction to Node.js and GitHub Flow");
+
+console.log("=== Node.js File Operations Demo ===\n");
+
+// 1. Read File
+const fs = require('fs');
+
+console.log("1. Reading file...");
+fs.readFile('file.txt', 'utf8', function (err, data) {
+  if (err) throw err;
+  console.log("File content:");
+  console.log(data);
+});
