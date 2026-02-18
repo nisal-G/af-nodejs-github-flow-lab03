c76ccfe1c8c6dccba57bf91399ed9994e34ae820
Mona Lisa <nimgunathilaka021@gmail.com>
2026-02-18 20:51:38 +0530

    Add write file functionality

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ console.log("Lab 03 - Introduction to Node.js and GitHub Flow");
 
 console.log("=== Node.js File Operations Demo ===\n");
 
-// 1. Read File
 const fs = require('fs');
 
+// 1. Read File
 console.log("1. Reading file...");
 fs.readFile('file.txt', 'utf8', function (err, data) {
   if (err) throw err;
   console.log("File content:");
   console.log(data);
+  console.log();
+});
+
+// 2. Write to File
+console.log("2. Writing to file...");
+fs.writeFile('output.txt', 'Hello World! This is written by Node.js', function (err) {
+  if (err) throw err;
+  console.log('File saved successfully!');
 });
